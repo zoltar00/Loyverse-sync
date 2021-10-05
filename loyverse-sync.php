a76fc3b2b7111ab6cabb3f3ae9cdbbadcca842ae
zoltar00 <32526436+zoltar00@users.noreply.github.com>
2021-10-05 23:41:55 +0200

    put all of code in Class

--- loyverse-sync.php
+++ loyverse-sync.php
@@ -25,6 +25,7 @@ Class LoyverseSyncPlugin {
 
         add_action('admin_menu', array($this,'adminPage'));
         add_action( 'admin_init', array($this,'settings'));
+        /*add_action('admin_menu', 'lvsync_create_menu');*/
 
     }
 
@@ -48,28 +49,11 @@ Class LoyverseSyncPlugin {
         <input type="text" name="<?php echo $args['theName'] ?>" value="<?php echo esc_attr(get_option($args['theName'])) ?>"></input>
 
     <?php }
-/*
-    function loyverseTokenHTML(){ ?>
 
-        <input type="text" name="lvs_lvtoken" value="<?php echo esc_attr(get_option('lvs_lvtoken')) ?>"></input>
-
-    <?php }
-
-    function WoocommerceTokenHTML(){ ?>
-
-        <input type="text" name="lvs_wckey" value="<?php echo esc_attr(get_option('lvs_wckey')) ?>"></input>
-
-    <?php }
-
-    function WoocommerceSecretHTML(){ ?>
-
-        <input type="text" name="lvs_wcsecret" value="<?php echo esc_attr(get_option('lvs_wcsecret')) ?>"></input>
-
-    <?php }
-*/
     function adminPage(){
 
         add_options_page('Loyverse Sync Settings','Loyverse Settings','manage_options','loyverse-sync-settings-page',array($this,'ourHTML'));
+        add_management_page('Loyverse sync', 'Loyverse sync', 'manage_options', 'loyverse-sync', array($this,'loyverse_sync'));
     
     }
     
@@ -89,21 +73,6 @@ Class LoyverseSyncPlugin {
     
     <?php }
 
-}
-
-$loyverseSyncPlugin = new LoyverseSyncPlugin();
-
-add_action('admin_menu', 'lvsync_create_menu');
-
-
-function lvsync_create_menu() {
-
-    add_management_page('Loyverse sync', 'Loyverse sync', 'manage_options', 'loyverse-sync', 'loyverse_sync');
-}
-
-
-
-$token = '8a9f63253d6c41e294e8f67d8ebcadea'; 
 
 function loyverse_categories_connection(){
 
@@ -113,10 +82,10 @@ function loyverse_categories_connection(){
     print_r($msg);
     echo '</br>'; 
 
-    global $token;
+    
     $responsecategories = wp_remote_retrieve_body(wp_remote_get('https://api.loyverse.com/v1.0/categories', array(
         'headers' => array(
-            'Authorization' => 'Bearer ' . $token
+            'Authorization' => 'Bearer ' . get_option('lvs_lvtoken','1')
         ),
     )));
     $data = json_decode($responsecategories,true);
@@ -133,10 +102,10 @@ function loyverse_items_connection(){
     echo "Connecting to Loyverse API...";
    
        /** Connect to Loyverse */
-       global $token;
+       
        $resp = wp_remote_retrieve_body(wp_remote_get('https://api.loyverse.com/v1.0/items', array(
            'headers' => array(
-               'Authorization' => 'Bearer ' . $token
+               'Authorization' => 'Bearer ' . get_option('lvs_lvtoken','1')
            ),
        )));
 
@@ -153,12 +122,12 @@ function loyverse_items_connection(){
 function get_loyverse_category_by_id($catid){
 
         /** Connect to loyverse */
-        global $token;
+
         $cat_url = 'https://api.loyverse.com/v1.0/categories/'.$catid;
 
         $responsecat = wp_remote_retrieve_body(wp_remote_get($cat_url, array(
             'headers' => array(
-                'Authorization' => 'Bearer ' . $token
+                'Authorization' => 'Bearer ' . get_option('lvs_lvtoken','1')
             ),
         )));
 
@@ -188,8 +157,8 @@ function loyverse_sync(){
 
 	$woocommerce = new Client(
 		'https://mammamia.mimlab.ch',
-		'ck_99b4d2a4d51cad847b882430b5406619528b8922',
-		'cs_ce509e4cb542d9dbfcba19c538961df957780290',
+		get_option('lvs_wckey','1'),
+		get_option('lvs_wcsecret','1'),
 		[
 			'wp_api' => true,
 			'version' => 'wc/v3',
@@ -206,7 +175,7 @@ function loyverse_sync(){
  /** Connect to Loyverse to get categories*/
   
 
- $loyverse_categories[] = loyverse_categories_connection();
+ $loyverse_categories[] = $this->loyverse_categories_connection();
 
  /** Get all categories from Woocommerce */
  
@@ -260,116 +229,121 @@ function loyverse_sync(){
  
  }    
 
-    $loyverse_items[] = loyverse_items_connection();/** Get items from Loyverse */
+        $loyverse_items[] = $this ->loyverse_items_connection();/** Get items from Loyverse */
 
-        
-   $msg="Got all Items...";
-   echo '<pre>'; 
-   print_r($msg);
-   echo '</br>'; 
+            
+    $msg="Got all Items...";
+    echo '<pre>'; 
+    print_r($msg);
+    echo '</br>'; 
 
-   $woocommerce_all_products = $woocommerce->get('products');
-   $wooprod = (array) $woocommerce_all_products;
+    $woocommerce_all_products = $woocommerce->get('products');
+    $wooprod = (array) $woocommerce_all_products;
 
-   foreach ($loyverse_items[0] as $loyverse_item) {
+    foreach ($loyverse_items[0] as $loyverse_item) {
 
-        foreach($loyverse_item as $item){
+            foreach($loyverse_item as $item){
 
 
-            $loyverse_item_slug = sanitize_title($item['item_name']);
-            $loyverse_item_name = $item['item_name'];
-            $loyverse_item_img_url = $item['image_url'];
-            $variant_sku = $item['variants'][0]['sku'];
-            $loyverse_item_price = $item['variants'][0]['default_price'];
-            $loyverse_catname = get_loyverse_category_by_id($item['category_id']); 
-            $loyverse_category_slug = sanitize_title($loyverse_catname);
+                $loyverse_item_slug = sanitize_title($item['item_name']);
+                $loyverse_item_name = $item['item_name'];
+                $loyverse_item_img_url = $item['image_url'];
+                $variant_sku = $item['variants'][0]['sku'];
+                $loyverse_item_price = $item['variants'][0]['default_price'];
+                $loyverse_catname = $this->get_loyverse_category_by_id($item['category_id']); 
+                $loyverse_category_slug = sanitize_title($loyverse_catname);
 
-            /** Get Woocommerce category id from loyverse category */
+                /** Get Woocommerce category id from loyverse category */
 
-            $woocommerce_all_categories = $woocommerce->get('products/categories');
-            $woocommerce_cats = (array)$woocommerce_all_categories;
+                $woocommerce_all_categories = $woocommerce->get('products/categories');
+                $woocommerce_cats = (array)$woocommerce_all_categories;
 
-            foreach($woocommerce_cats as $wccats){
+                foreach($woocommerce_cats as $wccats){
 
-            $found = 0;
+                $found = 0;
 
-                if($wccats->slug===$loyverse_category_slug){
-                
-                $wcid=$wccats->id;
+                    if($wccats->slug===$loyverse_category_slug){
+                    
+                    $wcid=$wccats->id;
 
-                $found = $found + 1;
+                    $found = $found + 1;
 
-                    break;
+                        break;
+
+                    }
 
                 }
 
-            }
+                /** Check if data already in Woocommerce */
+                $found = 0;
 
-              /** Check if data already in Woocommerce */
-            $found = 0;
+                foreach($wooprod as $prod){
 
-            foreach($wooprod as $prod){
+                        if($prod->slug===$loyverse_item_slug){
 
-                     if($prod->slug===$loyverse_item_slug){
+                            $msg = "Product ". $item['item_name'] ." already exists.";
+                            echo '<pre>'; 
+                            print_r($msg);
+                            echo '</br>';
+                            $found = $found + 1;
 
-                        $msg = "Product ". $item['item_name'] ." already exists.";
-                        echo '<pre>'; 
-                        print_r($msg);
-                        echo '</br>';
-                        $found = $found + 1;
+                            break;                      
 
-                        break;                      
-
-                    }
-            }
-                    if($found == 0){
-                        /** Create stuff for woocommerce product */
-                        $prod_data = [
-                            'name'          => $loyverse_item_name,
-                            'type'          => 'simple',
-                            'description'   => $loyverse_item_slug,
-                            'regular_price' => strval($loyverse_item_price),
-                            'sku' => $variant_sku,
-                            'categories' =>[
-                                [
-                                'id' => (integer)$wcid
+                        }
+                }
+                        if($found == 0){
+                            /** Create stuff for woocommerce product */
+                            $prod_data = [
+                                'name'          => $loyverse_item_name,
+                                'type'          => 'simple',
+                                'description'   => $loyverse_item_slug,
+                                'regular_price' => strval($loyverse_item_price),
+                                'sku' => $variant_sku,
+                                'categories' =>[
+                                    [
+                                    'id' => (integer)$wcid
+                                        
+                                    ]
                                     
+                                    ],
+                                'meta_data' => [
+                                        [
+                                            'key'=> '_knawatfibu_url',
+                                            'value'=> [
+                                                'img_url' => $loyverse_item_img_url,
+                                                'width'=> '500',
+                                                'height'=> '500'
+                                            ]
+                                        ]
                                 ]
                                 
-                                ],
-                            'meta_data' => [
-                                    [
-                                        'key'=> '_knawatfibu_url',
-                                        'value'=> [
-                                            'img_url' => $loyverse_item_img_url,
-                                            'width'=> '500',
-                                            'height'=> '500'
-                                        ]
-                                    ]
-                            ]
+                            ];
+
+                            $msg = "Sending item ". $loyverse_item_slug ." to woocommerce...";
+                            echo '<pre>'; 
+                            print_r($msg);
+                            echo '</br>'; 
                             
-                        ];
+                
+                            /**Send to WooCommerce */
+                                $woocommerce->post( 'products', $prod_data );
 
-                        $msg = "Sending item ". $loyverse_item_slug ." to woocommerce...";
-                        echo '<pre>'; 
-                        print_r($msg);
-                        echo '</br>'; 
-                        
-            
-                        /**Send to WooCommerce */
-                            $woocommerce->post( 'products', $prod_data );
+                        }
 
-                    }
-
+            }
         }
+
+        /**upload_image('https://api.loyverse.com/image/73804bed-0733-4701-9a06-55a8a613bb7b');*/
+
+        $msg= "Done importing!";
+        echo '<pre>'; 
+        print_r($msg);
+        echo '</br>'; 
     }
 
- /**upload_image('https://api.loyverse.com/image/73804bed-0733-4701-9a06-55a8a613bb7b');*/
-
-$msg= "Done importing!";
-echo '<pre>'; 
-print_r($msg);
-echo '</br>'; 
 }
+
+
+$loyverseSyncPlugin = new LoyverseSyncPlugin();
 
 ?>
