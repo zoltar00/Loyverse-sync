1842ac2e392ae54cfb1929d5fc7768b8fb5cb0fa
zoltar00 <zoltar02@gmail.com>
2021-11-27 01:59:09 +0100

    Fixed Update  deleted categories

--- loyverse-sync.php
+++ loyverse-sync.php
@@ -705,13 +705,16 @@ function loyverse_sync(){ ?>
 
     //print_r(count($loyverse_categories));
 
+
     foreach($loyverse_categories[0] as $loyverse_category){
-        
+
+        $found = 0;
+        $error = 0;
+
         foreach($loyverse_category as $category){
 
             //print($category['name']);
-            $found = 0;
-            $error = 0;
+            
 
             $loyverse_category_id = $category['id'];
   
@@ -741,12 +744,38 @@ function loyverse_sync(){ ?>
 
                                 //echo $e->getMessage();
                                 ?>        
-                                <pre> Category <?php echo $category['name'] ?> does not exist in Woocommerce but exists in the database. Delete it from the database <?php echo $tablename ?> and try synching again. </pre>
-                            <?php 
+                                    <pre> Category <?php echo $category['name'] ?> does not exist in Woocommerce but exists in the database. Putting it back. </pre>
+                                <?php 
                                 //$this ->write_to_loyverse_sync_log($e->getMessage());
-                                $this ->write_to_loyverse_sync_log('Category ' . $category['name'] . ' does not exist in Woocommerce. Delete it from the database ' . $tablename . ' and try synching again.');
-                                $error = 1;
+                                $this ->write_to_loyverse_sync_log('Category ' . $category['name'] . ' does not exist in Woocommerce. Putting it back.');
+                                
+                                
+                                ?>        
+                                    <pre> Sending category <?php echo $category['name'] ?> to woocommerce... </pre>
+                                <?php    
+            
+                            $this ->write_to_loyverse_sync_log('Sending category '.$category['name'].' to woocommerce... ');
+                            try{
+                            $woocommerce->post( 'products/categories', $prod_data );  
+                            $wpdb->update( $thedatabase , $db_data, array( 'lv_id' => $category['id'] )); 
+                            }
+                            catch(Exception $e){
+                                ?>        
+                                <pre> Category <?php echo $category['name'] ?> already exists in Woocommerce. Skipping. </pre>
+                            <?php    
+    
+                                $this ->write_to_loyverse_sync_log('Category '.$category['name'].' already exists in Woocommerce. Skipping. ');  
+                                $error = 1; 
                                 break;
+
+                            }
+                            ?>        
+                                <pre> Category <?php echo $category['name'] ?> sent to woocommerce... </pre>
+                            <?php    
+    
+                                $this ->write_to_loyverse_sync_log('Category '.$category['name'].' sent to woocommerce... ');                            
+                            $error = 1;
+                            break;
                                 
                             }
                             $wpdb->update( $thedatabase , $db_data, array( 'lv_id' => $category['id'] ));
@@ -779,7 +808,9 @@ function loyverse_sync(){ ?>
 
                 $this ->write_to_loyverse_sync_log('Sending category '.$category['name'].' to woocommerce... ');
 
-                $woocommerce->post( 'products/categories', $prod_data );
+
+                    $woocommerce->post( 'products/categories', $prod_data );
+
                 (array) $thecategory = $woocommerce->get('products/categories',['search' => sanitize_title($category['name'])]);
             
                 $data = array(
